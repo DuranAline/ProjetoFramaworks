4cc6266f8da01e4478d02d355e748305425798e9
Aline Duran <aline.duran@estudante.ifms.edu.br>
2023-10-29 17:47:53 -0400

    correcao app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react';
 import CommentForm from './components/CommentForm';
 import CommentList from './components/CommentList';
 import Header from './components/Header';
-import './App.css'; // Importe o arquivo de estilos para o App
+import './App.css';
 
 const App: React.FC = () => {
   const [comments, setComments] = useState<Comment[]>([]);
 
+  // Carregar comentários do armazenamento local quando o aplicativo é montado
   useEffect(() => {
     const storedComments = JSON.parse(localStorage.getItem('comments') || '[]');
     setComments(storedComments);
   }, []);
 
+  // Adicionar um novo comentário
   const addComment = (author: string, text: string) => {
     const newComment = {
       author,
@@ -21,6 +23,7 @@ const App: React.FC = () => {
     setComments([newComment, ...comments]);
   };
 
+  // Salvar os comentários no armazenamento local sempre que houver uma mudança
   useEffect(() => {
     localStorage.setItem('comments', JSON.stringify(comments));
   }, [comments]);
@@ -28,7 +31,7 @@ const App: React.FC = () => {
   return (
     <div>
       <Header />
-      <div className="content"> {/* Crie uma div para o conteúdo, se necessário */}
+      <div className="content">
         <h1>Diário de Comentários</h1>
         <CommentForm onAddComment={addComment} />
         <p>Total de Comentários: {comments.length}</p>
